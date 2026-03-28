0746c596a996af3deef4d3d9311cef7f663323a2
Disha Ghosh <lutinobudgie@proton.me>
2026-03-28 10:33:46 +0000

    finished create-users2 with dry run logic

--- create-users2.py
+++ create-users2.py
@@ -13,16 +13,22 @@ import re
 import sys
 
 def main():
+	#ask user whether to do a dry run or real run
+	dry_run_input = input("Dry run? (Y/N): ").strip().lower()
+	#if user enters Y, commands are printed but not executed
+	#if user enters N, the script runs the commands and creates users
+	dry_run = True if dry_run_input == 'y' else False #makes it safer to test the script before making system changes
 	#loop through each line from the input file (via stdin)
-	for line in sys.stdin:
+	for line in open("create-users.input"):
 		#check if the line starts with '#' (used for comments/skip lines in input file)
 		match = re.match("^#", line)
 		#split the line into fields using ':' as delimiter
 		fields = line.strip().split(':')
 		#skip lines that are comments OR do not have exactly 5 fields (invalud input)
 		if match or len(fields) != 5:
+			if dry_run:
+				print("Skipping invalid or comment line:", line.strip())
 			continue
-
 
 		#extract user data from fields (maps to entries in /etc/passwd)
 		username = fields[0]
@@ -35,17 +41,21 @@ def main():
 		#print status msg for user creation
 		print("==> Creating account for %s..." % (username))
 		cmd = "/usr/sbin/adduser --disabled-password --gecos '%s' %s" % (gecos, username)
-
-		print(cmd)
-		os.system(cmd)
+		
+		if dry_run:
+			print(cmd)
+		else:
+			os.system(cmd)
+		
 
 		#build command to set user password using echo + passwd
 		print("==> Setting the password for %s..." % (username))
 		cmd = "/bin/echo -ne '%s\n%s' | /usr/bin/sudo /usr/bin/passwd %s" % (password, password, username)
 
-		#for dry run: print command instead of executing
-		print(cmd)
-		os.system(cmd)
+		if dry_run:
+			print(cmd)
+		else:
+			os.system(cmd)
 
 		#loop thru each group and assign user
 		for group in groups:
@@ -53,8 +63,11 @@ def main():
 			if group != '-' :
 				print("==> Assigning %s to the %s group..." % (username,group))
 				cmd = "/usr/sbin/adduser %s %s" % (username, group)
-				print(cmd)
-				os.system(cmd)
+				
+				if dry_run:
+					print(cmd)
+				else:
+					os.system(cmd)
 
 #run main function
 if __name__ == '__main__':
